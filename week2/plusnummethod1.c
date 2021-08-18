2221a60be4d6f4eea2f2c7cc802071d626f18e53
MMaddoxx <nontouch16974@gmail.com>
2021-08-18 15:17:48 +0700

    Finish
    โปรแกรมหาตัวเลขที่มีค่ามากที่สุด (Level 1) โปรแกรมที่ 1

---
+++ week2/plusnummethod1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>//64011160 นนทัช มุกลีมาศ 
+int main()
+{
+    int num[3],i;
+    for(i=0;i<=2;i++)
+    {
+        printf("Enter number of number %d : ",i+1);
+        scanf("%d",&num[i]);
+    }
+    if(num[0]>num[1]&&num[0]>num[2])//ใช้วิธีเปรียบเทียบทีละตัว
+    {
+        printf("%d is the greatest number",num[0]);
+    }
+    else if(num[1]>num[0]&&num[1]>num[2])
+    {
+        printf("%d is the greatest number",num[1]);
+    }
+    else if(num[2]>num[0]&&num[2]>num[1])
+    {
+        printf("%d is the greatest number",num[2]);
+    }
+}

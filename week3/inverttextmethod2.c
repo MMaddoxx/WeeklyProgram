b59539f3c873f817bfc15f98525d7334c4e0dcab
MMaddoxx <nontouch16974@gmail.com>
2021-08-19 15:17:10 +0700

    Finish
    โปรแกรมรับ string แล้วแสดงจากหลังมาหน้า วิธีที่ 2

---
+++ week3/inverttextmethod2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()//ใช้วิธีสลับstring 
+{
+    char text[99],a;
+    int i;
+    printf("Enter text : ");
+    scanf("%[^\n]s",text);
+    for(i=0;i<=(strlen(text)-1)/2;i++)
+    {
+        a=text[i];//เอามาฝากไว้ที่a
+        text[i]=text[strlen(text)-1-i];//สลับ
+        text[strlen(text)-1-i]=a;//เอาตัวที่ฝากไว้มาใส้่
+    }
+    printf("%s",text);
+}

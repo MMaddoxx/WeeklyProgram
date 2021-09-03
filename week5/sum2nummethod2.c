4528c0ebb1cad8a7c3479d6c8fb2467294f7bdac
MMaddoxx <nontouch16974@gmail.com>
2021-09-03 13:02:43 +0700

    Finish
    Detail Fix

--- week5/sum2nummethod2.c
+++ week5/sum2nummethod2.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
     int num[3],i;
@@ -7,20 +8,24 @@ int main()
     {
         scanf("%d",&num[i]);
     }
-    if((num[0]>num[1]&&num[1]>num[2])||(num[1]>num[0]&&num[0]>num[2]))
+    if((num[0]>=num[1]&&num[1]>=num[2])||(num[1]>=num[0]&&num[0]>=num[2]))
     {
-        printf("Sum is %d",num[0]+num[1]);
+        printf("Sum Of 2 greatest number is %d",num[0]+num[1]);
+        exit(0);
     }
-    if((num[0]>num[2]&&num[2]>num[1])||(num[2]>num[0]&&num[0]>num[1]))
+    if((num[0]>=num[2]&&num[2]>=num[1])||(num[2]>=num[0]&&num[0]>=num[1]))
     {
-        printf("Sum is %d",num[0]+num[2]);
+        printf("Sum Of 2 greatest number is %d",num[0]+num[2]);
+        exit(0);
     }
-    if((num[1]>num[2]&&num[2]>num[0])||(num[2]>num[1]&&num[1]>num[0]))
+    if((num[1]>=num[2]&&num[2]>=num[0])||(num[2]>=num[1]&&num[1]>=num[0]))
     {
-        printf("Sum is %d",num[2]+num[1]);
+        printf("Sum Of 2 greatest number is %d",num[2]+num[1]);
+        exit(0);
     }
-    if((num[0]>num[2]&&num[2]>num[1])||(num[2]>num[0]&&num[0]>num[1]))
+    if((num[0]>=num[2]&&num[2]>=num[1])||(num[2]>=num[0]&&num[0]>=num[1]))
     {
-        printf("Sum is %d",num[0]+num[2]);
+        printf("Sum Of 2 greatest number is %d",num[0]+num[2]);
+        exit(0);
     }
 }

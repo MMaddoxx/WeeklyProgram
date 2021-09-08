58daaa0d76baf92aa42149c06d6ee0e5412fb75e
MMaddoxx <nontouch16974@gmail.com>
2021-09-08 09:32:27 +0700

    Done
    จงเขียนโปรแกรมเพื่อรับข้อความ 1 ข้อความแล้วให้โปรแกรมแสดงผลลัพธ์ว่ามีตัวอักษรตัวพิมพ์ใหญ่กี่ตัว ตัวพิมพ์เล็กกี่ตัว วิธีที่1 สร้างเว้ในmain

---
+++ week6/uplowmethod1.c
@@ -0,0 +1,22 @@
+//จงเขียนโปรแกรมเพื่อรับข้อความ 1 ข้อความแล้วให้โปรแกรมแสดงผลลัพธ์ว่ามีตัวอักษรตัวพิมพ์ใหญ่กี่ตัว ตัวพิมพ์เล็กกี่ตัว
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int i,upper=0,lower=0;
+    char text[100];
+    printf("Enter text : ");
+    scanf("%s",text);
+    for(i=0;i<strlen(text);i++)
+    {
+        if(text[i]>='A'&&text[i]<='Z')
+        {
+            upper++;
+        }
+        else 
+        {
+            lower++;
+        }
+    }
+    printf("There is %d uppercase and %d lowercase ",upper,lower);
+}
